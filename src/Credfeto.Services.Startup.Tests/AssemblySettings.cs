d5a29b56ea38041114d9da8183123f56f4387b3b
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-11-24 16:00:57 +0000

    Fixed code analysis issues

--- src/Credfeto.Services.Startup.Tests/AssemblySettings.cs
+++ src/Credfeto.Services.Startup.Tests/AssemblySettings.cs
@@ -1,3 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
-
+[assembly:
+    SuppressMessage(category: "Philips.CodeAnalysis.MaintainabilityAnalyzers",
+                    checkId: "PH2140: Avoid ExcludeFromCodeCoverage",
+                    Justification = "This is a unit test assembly - no need for coverage of the test code itself")]
 [assembly: ExcludeFromCodeCoverage]
